a3074a778ca168f135181cc801b65aec06106dcd
Quan Minh <quanminh2019@gmail.com>
2023-01-29 22:36:45 +0700

    Create Products preview carousel on Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,12 @@
 import { About } from "../../components/About";
 import Banner from "../../components/Banner";
+import { ProductsPreview } from "../../components/ProductsPreview";
+
 const Home = () =>{
     return(
         <>
             <Banner/>
+            <ProductsPreview/>
             <About/>
         </>
         

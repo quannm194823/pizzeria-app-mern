5404771b3e0fab3c4fd97c2c1d97e450a472981d
Quan Minh <quanminh2019@gmail.com>
2023-01-29 21:58:23 +0700

    Populate database using fakerjs package

--- server/index.js
+++ server/index.js
@@ -21,7 +21,10 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to my App"})
 })
 
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+app.use('/api/', productRouter);

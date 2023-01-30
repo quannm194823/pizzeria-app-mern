55f9bf4081dee1e0c3be34fc02c4591e12c499b0
Quan Minh <quanminh2019@gmail.com>
2023-01-30 17:30:48 +0700

    Create Menu page

--- src/stores/rootReducer.js
+++ src/stores/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import cartReducer from "./cart/cartSlice";
+import productReducer from "./menu/productsSlice";
 
 const rootReducer = combineReducers(
-    {cart: cartReducer}
+    {
+        cart: cartReducer,
+        products: productReducer
+    }
 )
 
 export default rootReducer;

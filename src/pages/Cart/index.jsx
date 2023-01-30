56758a2442983a79bfc69ccad50c312f632e6d68
Quan Minh <quanminh2019@gmail.com>
2023-01-30 22:35:29 +0700

    Create Summary tab for Cart page

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -5,7 +5,7 @@ import { cartProducts } from "../../stores/cart/cartSlice";
 import useTabSwitch from "../../hooks/useTabSwitch";
 import { ReactComponent as ArrowRightSvg } from "../../assets/icons/arrow-right-long-svgrepo-com.svg";
 import { AddressForm } from "../../components/AddressForm";
-// import { ProductsSummary } from "../../components/ProductsSummary";
+import { ProductsSummary } from "../../components/ProductsSummary";
 // import { StripeWrapper } from "../../components/PaymentForm";
 
 const Cart = () => {
@@ -25,7 +25,10 @@ const Cart = () => {
         <div className="bg-white h-screen text-black mx-auto mt-2 border border-gray-200 p-4 md:w-2/3 rounded-lg shadow-md sm:p-6 lg:p-8">
             <Tabs list={tabs} onTabSwitch={handleTabSwitch} activeTab={currentTab} />
             <div className={`tabs ${currentTab !== 'Summary' ? 'hidden' : ''}`}>
-                Summary
+                <ProductsSummary/>
+                <div className="flex justify-end p-2">
+                    <Button variant="dark" className="flex items-center" onClick={()=>handleTabSwitch('Delivery')}><span className="mr-1">Next</span><ArrowRightSvg /></Button>
+                </div>
             </div>
             <div className={`tabs ${currentTab !== 'Delivery' ? 'hidden' : ''}`}>
                 <AddressForm onTabSwitch={handleTabSwitch}/>
